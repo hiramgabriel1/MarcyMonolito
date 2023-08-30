3921fe8866b7e0e6c1f6ee0ea8e518be22d36349
itsrusty <rusty3031@gmail.com>
2023-08-29 18:59:42 -0700

    #2 config a cors

--- server/index.js
+++ server/index.js
@@ -29,11 +29,7 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: ["https://marcybackend.onrender.com","https://marcybackend.onrender.com/auth/login" ],
-  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-  credentials: true
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */

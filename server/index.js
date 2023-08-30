20f07363669599f38f535d36933810c828d3ed03
itsrusty <rusty3031@gmail.com>
2023-08-29 19:08:23 -0700

    test #24

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ dotenv.config();
 app.use(express.json());
 // app.use(helmet());
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+app.use(morgan("common"));  
 app.use(express.json({ limit: "600mb" }));
 app.use(express.urlencoded({ limit: "600mb", extended: true }));
 app.use(cors());

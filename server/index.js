90c976398f69e30261c15f3724cbd264cc02baca
itsrusty <rusty3031@gmail.com>
2023-08-29 19:04:52 -0700

    test #23

--- server/index.js
+++ server/index.js
@@ -27,8 +27,8 @@ app.use(express.json());
 // app.use(helmet());
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(express.json({ limit: "30mb" }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "600mb" }));
+app.use(express.urlencoded({ limit: "600mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
